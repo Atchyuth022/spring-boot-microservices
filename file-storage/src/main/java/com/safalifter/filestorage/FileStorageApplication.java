fec84010f85205a8a9ba244c357432dd4e7100a7
Atchyuth022 <atchyuthbevara@gmail.com>
2025-02-09 19:28:51 +0530

    dummy commit

--- file-storage/src/main/java/com/safalifter/filestorage/FileStorageApplication.java
+++ file-storage/src/main/java/com/safalifter/filestorage/FileStorageApplication.java
@@ -7,7 +7,8 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication
 @EnableEurekaClient
 public class FileStorageApplication {
-
+//sdsdsdsd
+	//sdsdsd
     public static void main(String[] args) {
         SpringApplication.run(FileStorageApplication.class, args);
     }
